a6a4e284b60db6ae46f8e934dfca8cd855244193
darthkolli145 <77557182+darthkolli145@users.noreply.github.com>
2024-06-26 22:54:29 -0700

    part 1 test

--- free_vocabulary_test.c
+++ free_vocabulary_test.c
@@ -5,3 +5,14 @@
 
 #include "wordle_lib.h"
 
+int main(void) {
+  printf("\n**** tests for free_vocabulary ****\n");
+  char **expected_vocabulary;
+  expected_vocabulary = calloc(20, sizeof(char *));
+  int i = 0;
+  expected_vocabulary[i++] = strdup("moody");
+  expected_vocabulary[i++] = strdup("sheep");
+  expected_vocabulary[i++] = strdup("hippy");
+  expected_vocabulary[i++] = strdup("sleet");
+  expected_vocabulary[i++] = strdup("oxide");
+  expected_vocabulary[i++] = strdup("flour");
