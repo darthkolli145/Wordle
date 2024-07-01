8881627820f6799284387866bc34594057da533b
darthkolli145 <77557182+darthkolli145@users.noreply.github.com>
2024-06-30 23:21:58 -0700

    helper function

--- wordle_lib_test.c
+++ wordle_lib_test.c
@@ -4,3 +4,9 @@
 #include <string.h>
 
 #include "wordle_lib.h"
+
+void should_be_exactly_equal(const char *message, int expected, int actual) {
+  printf("%s\n", message);
+  printf("%s: wanted %d, got %d\n",
+         (expected == actual) ? "SUCCESS" : "FAILURE", expected, actual);
+}
